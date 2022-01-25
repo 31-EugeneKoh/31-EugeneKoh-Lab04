82af7725636aec88ce70c69061e17736eb3559d2
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-01-25 12:11:01 +0800

    Added Materials, Player and Ground

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    Rigidbody PlayerRigidbody;
+    public Rigidbody PlayerRigidbody;
 
     // Start is called before the first frame update
     void Start()
