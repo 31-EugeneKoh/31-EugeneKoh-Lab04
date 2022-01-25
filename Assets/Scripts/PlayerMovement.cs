f5605b81a0263458e749cc055c70524156ebd8a4
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-01-25 12:30:24 +0800

    Polished PlayerMovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public Rigidbody PlayerRigidbody;
+    Rigidbody PlayerRigidbody;
+
+    int coinCount;
+    public Text coinText;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        PlayerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            StartRun();
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            StartRun();
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            StartRun();
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            StartRun();
+        }
     }
 
     void FixedUpdate()
@@ -25,5 +48,10 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         PlayerRigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void StartRun()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
